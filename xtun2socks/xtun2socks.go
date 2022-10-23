2efeec2d7e65c3f7073ee0f067e55c59ee431b66
Rosario <Rosario@google.com>
2022-10-23 13:08:10 +0800

    fix: tun2socks Optison

--- xtun2socks/xtun2socks.go
+++ xtun2socks/xtun2socks.go
@@ -6,14 +6,24 @@ import (
 	"go.uber.org/automaxprocs/maxprocs"
 )
 
-type Options = engine.Key
+type Options struct {
+	MTU      int
+	Proxy    string
+	LogLevel string
+	Device   string
+}
 
 func Start(opt *Options) {
 	ch := make(chan struct{})
 	go func() {
 		close(ch)
+		key := new(engine.Key)
+		key.MTU = opt.MTU
+		key.Device = opt.Device
+		key.LogLevel = opt.LogLevel
+		key.Proxy = opt.Proxy
 		maxprocs.Set(maxprocs.Logger(func(s string, i ...interface{}) {}))
-		engine.Insert(opt)
+		engine.Insert(key)
 		engine.Start()
 	}()
 	<-ch
